474b662745845751be9ac7a2f425f96c0a35d6b1
winsonluk <winson.luk@gmail.com>
2018-11-03 11:49:04 -0700

    add emojis to HappyPicker.jsx, add firebase to index.js

--- src/src/index.js
+++ src/src/index.js
@@ -10,3 +10,45 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+var firebase = require("firebase");
+
+var config = {
+  apiKey: "AIzaSyA_-C7Ns-wMUd_JlKPCevlYr1K2J8L2_90",
+  authDomain: "hapsadhapsad.firebaseapp.com",
+  databaseURL: "https://hapsadhapsad.firebaseio.com",
+  projectId: "hapsadhapsad",
+  storageBucket: "hapsadhapsad.appspot.com",
+  messagingSenderId: "143800903151"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.firestore();
+
+var docRef = db.collection('users').doc('alovelace');
+
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+var aTuringRef = db.collection('users').doc('aturing');
+
+var setAlan = aTuringRef.set({
+  'first': 'Alan',
+  'middle': 'Mathison',
+  'last': 'Turing',
+  'born': 1912
+});
+
+db.collection('users').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
