4bf34504e25a84818f6cc8ff999e2d4609ff7419
Stephen Jayakar <aj12ay@gmail.com>
2018-11-03 19:24:40 -0700

    mVP

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
 
 import PostCell from './PostCell';
 import SearchBar from './SearchBar';
-import { readUserPosts } from '../index';
+import { readAllPosts } from '../index';
+
+TimeAgo.locale(en);
+
+const timeAgo = new TimeAgo('en-US');
 
 class Feed extends React.Component {
 	constructor(props) {
@@ -14,7 +20,7 @@ class Feed extends React.Component {
 			posts: []
 		};
 
-		readUserPosts().then((posts) => {
+		readAllPosts().then((posts) => {
 			this.setState({posts});
 		});
 	}
@@ -27,14 +33,29 @@ class Feed extends React.Component {
 				{posts.map((post) => (
 					<PostCell
 						text={post.text}
-						userId={post.userId}
-						timestamp={post.timestamp}
-						emotion={post.score}
+						userId={post.name}
+						timestamp={timeAgo.format(post.timestamp)}
+						emotion={scoreToEmoji(post.score)}
 					/>
 				))}
 			</div>
 		);
 	}
+}
+
+const scoreToEmoji = (score) => {
+	switch (score) {
+		case 'a':
+			return '😭';
+		case 'b':
+			return '😞';
+		case 'c':
+			return '🙂';
+		case 'd':
+		  return '🙂';
+		default:
+		  return score;
+	}	
 }
 
 const mapStateToProps = (state) => ({
